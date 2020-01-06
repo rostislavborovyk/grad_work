931f7adb166cd354bb5d5b453af4162558502ef7
Rostislav <rostix2000@gmail.com>
2020-01-06 14:11:39 +0200

    Fixed tests

--- department_app/config.py
+++ department_app/config.py
@@ -8,12 +8,12 @@ class BaseConfig:
     SECRET_KEY = '8ed308dc-4028-410b-a104-09bde4f507b9'
     SQLALCHEMY_DATABASE_URI = 'mysql://wxtuYnuHC0:FfjxV5hUB4@remotemysql.com:3306/wxtuYnuHC0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
 
 
 class TestConfig(BaseConfig):
     DEBUG = True
     TESTING = True
-    WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'mysql://wxtuYnuHC0:FfjxV5hUB4@remotemysql.com:3306/wxtuYnuHC0'
 
 
@@ -23,3 +23,4 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
+    WTF_CSRF_ENABLED = True
--- department_app/tests/test_api.py
+++ department_app/tests/test_api.py
@@ -1,10 +1,13 @@
 """
 Unittests for app
 """
+import sys
 
+print(sys.path)
 import unittest
 
 from department_app import app
+from department_app.views import *
 
 
 class AppTest(unittest.TestCase):
@@ -12,6 +15,17 @@ class AppTest(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/')
+        self.assertEqual(200, response.status_code)
+
+    def test_departments(self):
+        tester = app.test_client(self)
+        response = tester.get('/departments')
+        self.assertEqual(200, response.status_code)
+
+    def test_employees(self):
+        tester = app.test_client(self)
+        response = tester.get('/employees')
+        self.assertEqual(200, response.status_code)
 
 
 if __name__ == '__main__':
